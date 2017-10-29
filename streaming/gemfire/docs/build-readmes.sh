f59a5e39019b8d2471d88f2bc0b662b5a15d91e4
David Turanski <dturanski@pivotal.io>
2017-10-29 12:37:41 -0400

    Use coalesce.rb to generate READMEs

---
+++ streaming/gemfire/docs/build-readmes.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+DOCS_DIR=../../../docs
+$DOCS_DIR/coalesce.rb http-gemfire/README.adoc > ../http-gemfire/README.adoc
+$DOCS_DIR/coalesce.rb gemfire-log/README.adoc > ../gemfire-log/README.adoc

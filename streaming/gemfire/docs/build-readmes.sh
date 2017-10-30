89f54e2ff700447d58c29bf76423710a71ba2c57
David Turanski <dturanski@pivotal.io>
2017-10-30 18:12:37 -0400

    Build with mvn

--- streaming/gemfire/docs/build-readmes.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-DOCS_DIR=../../../docs
-$DOCS_DIR/coalesce.rb http-gemfire/README.adoc > ../http-gemfire/README.adoc
-$DOCS_DIR/coalesce.rb gemfire-log/README.adoc > ../gemfire-log/README.adoc
-$DOCS_DIR/coalesce.rb gemfire-cq-log/README.adoc > ../gemfire-cq-log/README.adoc

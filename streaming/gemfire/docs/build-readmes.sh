5549350254317d77243d3a2c6f3e8b45913eff92
David Turanski <dturanski@pivotal.io>
2017-10-30 10:19:53 -0400

    Add gemfire-cq-log

--- streaming/gemfire/docs/build-readmes.sh
+++ streaming/gemfire/docs/build-readmes.sh
@@ -2,3 +2,4 @@
 DOCS_DIR=../../../docs
 $DOCS_DIR/coalesce.rb http-gemfire/README.adoc > ../http-gemfire/README.adoc
 $DOCS_DIR/coalesce.rb gemfire-log/README.adoc > ../gemfire-log/README.adoc
+$DOCS_DIR/coalesce.rb gemfire-cq-log/README.adoc > ../gemfire-cq-log/README.adoc

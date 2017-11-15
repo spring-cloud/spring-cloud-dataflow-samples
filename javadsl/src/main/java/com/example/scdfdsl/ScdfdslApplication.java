3c1642f41f6b9f563048b9ff2c77f7600818fed1
Mark Pollack <mpollack@pivotal.io>
2017-11-15 16:16:28 -0500

    expand sample

--- javadsl/src/main/java/com/example/scdfdsl/ScdfdslApplication.java
+++ javadsl/src/main/java/com/example/scdfdsl/ScdfdslApplication.java
@@ -4,16 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.dataflow.rest.client.DataFlowOperations;
+import org.springframework.cloud.dataflow.rest.client.DataFlowTemplate;
 import org.springframework.cloud.dataflow.rest.client.dsl.Stream;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootApplication
 public class ScdfdslApplication implements CommandLineRunner {
-
-	@Autowired
-	private Stream.StreamNameBuilder builder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScdfdslApplication.class, args);
@@ -21,21 +21,31 @@ public class ScdfdslApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
-		System.out.println(builder);
+		URI dataFlowUri = URI.create("http://localhost:9393");
+		DataFlowOperations dataFlowOperations = new DataFlowTemplate(dataFlowUri);
+
+		dataFlowOperations.appRegistryOperations().importFromResource(
+				"http://bit.ly/Celsius-RC1-stream-applications-rabbit-maven", true);
 
 		Map<String, String> deploymentProperties = new HashMap<>();
-		deploymentProperties.put("app.splitter.producer.partitionKeyExpression",
-				"payload");
+		deploymentProperties.put("app.splitter.producer.partitionKeyExpression", "payload");
 		deploymentProperties.put("deployer.log.count", "2");
 
-		Stream woodchuck = builder.name("woodchuck").definition(
+		System.out.println("Deploying stream.");
+		Stream woodchuck = Stream.Builder(dataFlowOperations).name("woodchuck").definition(
 				"http --server.port=9900 | splitter --expression=payload.split(' ') | log")
 				.create().deploy(deploymentProperties);
 
 		while(!woodchuck.getStatus().equals("deployed")){
+			System.out.println("Wating for deployment of stream.");
 			Thread.sleep(5000);
 		}
 
+		System.out.println("Letting the stream run for 2 minutes.");
+		// Let woodchuck run for 2 minutes
+		Thread.sleep(120000*4);
+
+		System.out.println("Destroying stream");
 		woodchuck.destroy();
 	}
 }

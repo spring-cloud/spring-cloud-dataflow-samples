f163236bcbc88fb1b658e4aec7f438c9224f1fce
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2019-04-29 17:42:52 +0530

    Refactor samples

---
+++ dataflow-website/stream-developer-guides/streams/usage-cost-logger/src/main/java/io/spring/dataflow/sample/UsageCostLoggerApplication.java
@@ -0,0 +1,12 @@
+package io.spring.dataflow.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsageCostLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsageCostLoggerApplication.class, args);
+	}
+}

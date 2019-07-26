2d0c3cd5d77c85015834e1fbb411759dbe67a41b
Janne Valkealahti <janne.valkealahti@gmail.com>
2019-07-26 07:31:14 +0100

    Maven and Gradle samples for dataflow server
    - Minimal and fully working sample of building custom
      dataflow server adding mysql driver.

---
+++ custom-dataflow-builds/dataflow-server-21x/src/test/java/com/example/customdataflowserver/CustomDataflowServerApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.customdataflowserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = Replace.ANY)
+public class CustomDataflowServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

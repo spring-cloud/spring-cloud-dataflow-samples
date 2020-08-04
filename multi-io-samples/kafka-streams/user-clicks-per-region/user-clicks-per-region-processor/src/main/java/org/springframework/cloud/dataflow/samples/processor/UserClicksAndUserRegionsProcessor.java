4b0782d7a1c301df4f2aa8ed2ad4709fa203d099
Ilayaperumal Gopinathan <igopinathan@pivotal.io>
2020-08-04 17:02:40 +0530

    Add multi IO samples

---
+++ multi-io-samples/kafka-streams/user-clicks-per-region/user-clicks-per-region-processor/src/main/java/org/springframework/cloud/dataflow/samples/processor/UserClicksAndUserRegionsProcessor.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.dataflow.samples.processor;
+
+import java.util.function.BiFunction;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Joined;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Serialized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class UserClicksAndUserRegionsProcessor {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserClicksAndUserRegionsProcessor.class, args);
+	}
+
+	@Bean
+	public BiFunction<KStream<String, Long>, KTable<String, String>, KStream<String, Long>> processUserClickStream() {
+
+		return (userClicksStream, userRegionsTable) -> userClicksStream
+				.leftJoin(userRegionsTable,
+						(clicks, region) -> new RegionWithClicks(region == null ? "UNKNOWN" : region, clicks),
+						Joined.with(Serdes.String(), Serdes.Long(), null))
+				.map((user, regionWithClicks) -> new KeyValue<>(regionWithClicks.getRegion(), regionWithClicks.getClicks()))
+				.groupByKey(Serialized.with(Serdes.String(), Serdes.Long()))
+				.reduce((firstClicks, secondClicks) -> firstClicks + secondClicks)
+				.toStream();
+	}
+
+
+	private static final class RegionWithClicks {
+
+		private final String region;
+		private final long clicks;
+
+		public RegionWithClicks(String region, long clicks) {
+			if (region == null || region.isEmpty()) {
+				throw new IllegalArgumentException("region must be set");
+			}
+			if (clicks < 0) {
+				throw new IllegalArgumentException("clicks must not be negative");
+			}
+			this.region = region;
+			this.clicks = clicks;
+		}
+
+		public String getRegion() {
+			return region;
+		}
+
+		public long getClicks() {
+			return clicks;
+		}
+
+	}
+}
---
+++ multi-io-samples/ticktock/multi-output-time-kafka/src/main/java/org/springframework/cloud/stream/app/time/source/kafka/MultiOutputTimeSourceKafkaApplication.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2019-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.app.time.source.rabbit;
+
+import java.util.Date;
+import java.util.function.Supplier;
+
+import org.apache.commons.lang3.time.FastDateFormat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.fn.supplier.time.TimeSupplierProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+
+
+@SpringBootApplication
+@Import({ org.springframework.cloud.fn.supplier.time.TimeSupplierConfiguration.class })
+public class MultiOutputTimeSourceKafkaApplication {
+
+	@Bean
+	public Supplier<String> timeSupplier2(TimeSupplierProperties timeSupplierProperties) {
+		FastDateFormat fastDateFormat = FastDateFormat.getInstance(timeSupplierProperties.getDateFormat());
+		return () -> fastDateFormat.format(new Date());
+	}
+
+	@Bean
+	public Supplier<String> timeSupplier3(TimeSupplierProperties timeSupplierProperties) {
+		FastDateFormat fastDateFormat = FastDateFormat.getInstance(timeSupplierProperties.getDateFormat());
+		return () -> fastDateFormat.format(new Date());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiOutputTimeSourceKafkaApplication.class, args);
+	}
+}

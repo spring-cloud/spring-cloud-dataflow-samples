220f4641b639f8187bf6888c25c32fc2c240d5fd
Glenn Renfro <grenfro@pivotal.io>
2020-11-19 17:07:56 -0500

    Application that lets users test task/batch scenarios with one app
    resolves #140

---
+++ scenario-task/src/main/java/io/spring/scenariotask/ScenarioTaskApplication.java
@@ -0,0 +1,13 @@
+package io.spring.scenariotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScenarioTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScenarioTaskApplication.class, args);
+	}
+
+}
